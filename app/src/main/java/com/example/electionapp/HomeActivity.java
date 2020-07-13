297701c1b8dcc760c6166effd7c563d6076ad1b6
PRIYANK TRIPATHI <raunaktrip@gmail.com>
2020-07-13 16:19:21 +0530

    FireBase authentication

---
+++ app/src/main/java/com/example/electionapp/HomeActivity.java
@@ -0,0 +1,35 @@
+package com.example.electionapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+
+    Button btnLogout;
+    FirebaseAuth mFirrebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        btnLogout.findViewById(R.id.button2);
+
+
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intoSignUp= new Intent(HomeActivity.this,MainActivity.class);
+                startActivity(intoSignUp);
+            }
+        });
+    }
+}
